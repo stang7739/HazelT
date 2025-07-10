0bb2abe06bb374864833c9526297a23c9937b97a
tangsan <997462235@qq.com>
2025-07-10 14:18:29 +0800

    resize window and cameraControll

---
+++ Hazel/src/Hazel/Events/OrthographicCameraController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stang on 25-7-9.
+//
+
+#include "OrthographicCameraController.h"
---
+++ Hazel/src/Hazel/Events/OrthographicCameraController.h
@@ -0,0 +1,16 @@
+//
+// Created by stang on 25-7-9.
+//
+
+#ifndef ORTHOGRAPHICCAMERACONTROLLER_H
+#define ORTHOGRAPHICCAMERACONTROLLER_H
+
+
+
+class OrthographicCameraController {
+
+};
+
+
+
+#endif //ORTHOGRAPHICCAMERACONTROLLER_H
